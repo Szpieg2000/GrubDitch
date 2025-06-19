549fd309135be036c92ba394334451b916122caf
Szpieg2000 <Panhotdog@o2.pl>
2025-06-19 13:50:39 +0200

    Modal Text

--- Scripts/Info.js
+++ Scripts/Info.js
@@ -28,18 +28,40 @@ function ImageModalCreate() {
 	PageHolder.innerHTML = ModalCode + PageHolder.innerHTML
 	
 	var Modal = document.getElementById("Modal");
-		var ModalContent = document.getElementById("ModalContent");
+	var ModalContent = document.getElementById("ModalContent");
 	var ModalCaption = document.getElementById("ModalCaption");
 
-	function SetUpModalImage(img) {
+	document.querySelectorAll("img.ModalImage").forEach(function(img) {
 		img.onclick = function(){
 			Modal.style.display = "block";
 			ModalContent.src = this.src;
+			ModalContent.style.visibility = "visible"
 			ModalCaption.innerHTML = this.alt;
+			
+			ModalContent.style.height = "80%";
+			ModalContent.style["max-width"] = "700px";
+			
+			ModalCaption.style.height = "150px";
+			ModalCaption.style.width = "80%";
+			ModalCaption.style["font-size"] = "100%";
 		}
-	}
-
-	document.querySelectorAll("img.ModalImage").forEach(SetUpModalImage)
+	})
+	
+	document.querySelectorAll("[data-ModalText]").forEach(function(Text) {
+		Text.onclick = function(){
+			Modal.style.display = "block";
+			ModalContent.style.visibility = "hidden"
+			ModalCaption.innerHTML = this.dataset.modaltext;
+			
+			ModalContent.style.height = "0";
+			ModalContent.style["max-width"] = "0";
+			
+			ModalCaption.style.height = "100%";
+			ModalCaption.style.width = "100%";
+			ModalCaption.style["font-size"] = "150%";
+  
+		}
+	})
 	
 	var span = document.getElementsByClassName("CloseModalImage")[0];
 	span.onclick = function() {
