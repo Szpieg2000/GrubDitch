b8a2e4d5015d025e217b1208042fd51879a09d07
Szpieg2000 <Panhotdog@o2.pl>
2025-06-19 12:56:51 +0200

    Modal For Images

--- Scripts/Info.js
+++ Scripts/Info.js
@@ -17,4 +17,38 @@ function TopBarCreate() {
 		
 	PageHolder.innerHTML = TopBarCode + PageHolder.innerHTML
 }
+
+function ImageModalCreate() {
+	var PageHolder =  document.getElementById("PageHolder")
+	
+	let ModalCode = '<div id="Modal" class="modal">'
+	ModalCode = ModalCode + '<span class="CloseModalImage">&times;</span>'
+	ModalCode = ModalCode + '<img class="modal-content" id="ModalContent">'
+	ModalCode = ModalCode + '<div id="ModalCaption"></div></div>'
+	PageHolder.innerHTML = ModalCode + PageHolder.innerHTML
+	
+	var Modal = document.getElementById("Modal");
+		var ModalContent = document.getElementById("ModalContent");
+	var ModalCaption = document.getElementById("ModalCaption");
+
+	function SetUpModalImage(img) {
+		img.onclick = function(){
+			Modal.style.display = "block";
+			ModalContent.src = this.src;
+			ModalCaption.innerHTML = this.alt;
+		}
+	}
+
+	document.querySelectorAll("img.ModalImage").forEach(SetUpModalImage)
+	
+	var span = document.getElementsByClassName("CloseModalImage")[0];
+	span.onclick = function() {
+		Modal.style.display = "none";
+	}
+		
+}
+	
 TopBarCreate()
+ImageModalCreate()
+
+
